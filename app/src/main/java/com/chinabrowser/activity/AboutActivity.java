b0d7a1afe59f4ca46504ee345a2c00a2ead4ca06
CherryLu <15551283679@163.com>
2018-05-09 18:09:26 +0800

    正式打包文字

--- app/src/main/java/com/chinabrowser/activity/AboutActivity.java
+++ app/src/main/java/com/chinabrowser/activity/AboutActivity.java
@@ -1,8 +1,14 @@
 package com.chinabrowser.activity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.chinabrowser.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2018/4/14.
@@ -10,9 +16,32 @@ import com.chinabrowser.R;
 
 public class AboutActivity extends BaseActivity {
 
+    @Bind(R.id.name)
+    TextView name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        String str = getString(R.string.app_name)+getVerName(this);
+        name.setText(str);
+    }
+
+    /**
+     * 获取版本号名称
+     *
+     * @param context 上下文
+     * @return
+     */
+    public static String getVerName(Context context) {
+        String verName = "";
+        try {
+            verName = context.getPackageManager().
+                    getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return verName;
     }
 }
