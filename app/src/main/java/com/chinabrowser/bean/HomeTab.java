ce847de557a3223381adbeba015735fe5f99783d
CherryLu <g15551283679@163.com>
2018-06-04 23:27:40 +0800

    最后修改！！！

--- app/src/main/java/com/chinabrowser/bean/HomeTab.java
+++ app/src/main/java/com/chinabrowser/bean/HomeTab.java
@@ -12,6 +12,20 @@ public class HomeTab implements Serializable{
     public String title;
     public Bitmap bitmap;
 
+    public int type;
+
+    public Title titleData;// LISTPAG
+    public String newsId;
+    public String url;
+
+    public static int HOMEPAGE = 0;
+    public static int LISTPAG = 1;
+    public static int NEWSPAGE = 2;
+    public static int TRANSLATEPAGE = 3;
+    public static int URLWEBPAGE = 4;
+    public static int SEARCHPAGE = 5;
+
+
 
 
 }
